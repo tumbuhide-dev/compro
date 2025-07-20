2d78ff4b577405736a5478a87a93eecab45e427f
tumbuhide-dev <tumbuhide.dev@gmail.com>
2025-07-20 23:51:23 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -9,20 +9,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     pageFlip.loadFromImages([
-        "images/compro_0001.jpg",
-        "images/compro_0002.jpg",
-        "images/compro_0003.jpg",
-        "images/compro_0004.jpg",
-        "images/compro_0005.jpg",
-        "images/compro_0006.jpg",
-        "images/compro_0007.jpg",
-        "images/compro_0008.jpg",
-        "images/compro_0009.jpg",
-        "images/compro_0010.jpg",
-        "images/compro_0011.jpg",
-        "images/compro_0012.jpg",
-        "images/compro_0013.jpg",
-        "images/compro_0014.jpg",
-        "images/compro_0015.jpg"
+        "./images/compro_0001.jpg",
+        "./images/compro_0002.jpg",
+        "./images/compro_0003.jpg",
+        "./images/compro_0004.jpg",
+        "./images/compro_0005.jpg",
+        "./images/compro_0006.jpg",
+        "./images/compro_0007.jpg",
+        "./images/compro_0008.jpg",
+        "./images/compro_0009.jpg",
+        "./images/compro_0010.jpg",
+        "./images/compro_0011.jpg",
+        "./images/compro_0012.jpg",
+        "./images/compro_0013.jpg",
+        "./images/compro_0014.jpg",
+        "./images/compro_0015.jpg"
     ]);
 });
